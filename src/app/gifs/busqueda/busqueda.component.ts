2fea7e1e6599ae761b11a3a1bfba6dd8a515ff56
DenisRodMor <104526631+DenisRodMor@users.noreply.github.com>
2022-06-22 14:27:09 -0600

    Agregando GifsService y mostrando en el sidebar la busqueda

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -11,10 +12,14 @@ export class BusquedaComponent  {
   // Para limpiar el input luego de presionar enter
   @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement> ;
 
+  constructor(private gifsservice: GifsService){
+
+  }
+
   buscar(){
     // console.log(this.txtBuscar);
    const valor =  this.txtBuscar.nativeElement.value;
-   console.log(valor);
+   this.gifsservice.buscarGifs(valor);
    this.txtBuscar.nativeElement.value= '';
   }
 
