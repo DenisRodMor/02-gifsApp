66e8bd7f853382cefe3e426dabb5eb60e7fb0c2f
DenisRodMor <104526631+DenisRodMor@users.noreply.github.com>
2022-06-22 08:41:00 -0600

    Creacion del GifsModule y sus componentes busqueda y resultados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
