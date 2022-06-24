89984c617527c69db61a1247e9379c6f9d052166
DenisRodMor <104526631+DenisRodMor@users.noreply.github.com>
2022-06-24 08:35:45 -0600

    Agregando LocalStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -19,7 +19,16 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor(private http : HttpClient){}
+  constructor(private http : HttpClient){
+
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || []
+
+
+    // Esta manera y la de arriba, ambas sirven para que muestre el historial de busqueda que esta en el localstorage
+    // if( localStorage.getItem('historial')){
+    //   this._historial = JSON.parse(localStorage.getItem('historial')!);
+    // }
+  }
 
   buscarGifs(query : string = '' ){
 
@@ -28,6 +37,9 @@ export class GifsService {
     if(!this._historial.includes(query)){ //el signo de admiracion al inicio es para decir decir la negacion de esa condicion
       this._historial.unshift(query); //si no lo incluye ahi si lo debe insertar
       this._historial = this._historial.splice(0,10);//para que unicamente muestre los 10 mas recientes en el historial
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+
     }
 
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=L0skS4P4ZsfqLII7u9wtgLe6FBQqbzir&q=${ query }&limit =10`)
